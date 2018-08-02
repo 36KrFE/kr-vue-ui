917932a27b02e7704e9519216ff591e3e336a680
高娜娜 <gaonana@jingdata.com>
2018-08-02 14:50:28 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello" @click="show">
+    hello
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      name: 'Amy'
+    }
+  },
+  methods: {
+    show() {
+      const arr = [12,3,2,14,34];
+      let newArr = arr.filter(item => {
+        return item > 5;
+      });
+      console.log(newArr);
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+  .hello {
+    font-size: 20px;
+    color: red;
+  }
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,100 @@
+var path = require('path')
+var webpack = require('webpack')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+// const { VueLoaderPlugin } = require ('vue-loader');
+
+const NODE_ENV = process.env.NODE_ENV
+
+module.exports = {
+  mode: 'production',
+  // 修改打包入口
+  entry: NODE_ENV == 'development' ? './src/main.js' : './index.js',
+
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'ke-vue-ui.js',
+    library: 'ke-vue-ui', // 指定的就是你使用require时的模块名
+    libraryTarget: 'umd', // 以何种格式打包。libraryTarget会生成不同umd的代码,可以只是commonjs标准的，也可以是指amd标准的，也可以只是通过script标签引入的
+    umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      '@': path.resolve(__dirname,'src'),
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+            // the "scss" and "sass" values for the lang attribute to the right configs here.
+            // other preprocessors should work out of the box, no loader config like this necessary.
+            'less': [
+              'vue-style-loader',
+              'css-loader',
+              'less-loader'
+            ],
+            
+          }
+          // other vue-loader options go here
+        }
+      },
+      {
+        // 以.js结尾的文件 会被处理
+        test: /\.js$/,
+        // 是用babel-loader进行处理
+        loader: 'babel-loader',
+        // 排除 node_modules里面的js文件
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+    overlay: true
+  },
+  performance: {
+    hints: false
+  },
+  // https://www.cnblogs.com/wangyingblog/p/7027540.html  cheap-source-map 生成一个没有列信息的SourceMaps文件，不包含loader的sourcemap
+  // devtool: '#cheap-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    // 定义全局
+    // 在开发环境中存在的提示信息，在生产环境中会去除，不会加载到代码中。可以减少代码量。
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new UglifyJsPlugin(),
+    // new VueLoaderPlugin(),
+    // 配置各个loader的插件，如postcss的 autoprefixer等；
+    new webpack.LoaderOptionsPlugin({
+      minimize: true   //加载器是否要切换到优化模式。
+    })
+  ])
+}
